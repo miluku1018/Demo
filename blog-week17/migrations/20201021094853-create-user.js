d18d9af2952387661768630b6f419457bbc99b59
miluku1018 <miluku1018@gmail.com>
2020-10-26 19:17:02 +0800

    update week17

--- blog-week17/migrations/20201021094853-create-user.js
+++ blog-week17/migrations/20201021094853-create-user.js
@@ -8,7 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userame: {
+      username: {
+        allowNull: false,
+        unique: true,
         type: Sequelize.STRING
       },
       password: {
